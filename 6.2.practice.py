0267da4f667c21b3165ac5824d6f64ec42819342
hojin1845 <hojin1845@naver.com>
2022-08-24 23:09:09 +0900

    2022-08-24 조건문 퀴즈

---
+++ 6.2.practice.py
@@ -0,0 +1,10 @@
+#6.2.for
+#print("대기번호 : 1")
+#print("대기번호 : 2")
+#print("대기번호 : 3")
+#print("대기번호 : 4")
+
+# randrange()
+for waiting_no in range(1, 6): # 0, 1, 2, 3, 4, 5
+    print("대기번호 : {0}".format(waiting_no))
+
