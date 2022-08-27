1b9cd6624f68bacb3be4500f980360dc102d86e6
hojin1845 <hojin1845@naver.com>
2022-08-27 14:10:04 +0900

    continue와break

--- 6.4.practice.py
+++ 6.4.practice.py
@@ -7,4 +7,4 @@ for student in range(1, 11): # 1,2,3,4,5,6,7,8,9,10
     elif student in no_book:
         print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
         break
-    print("{0}, 책을 읽어봐".format(student))
+    
