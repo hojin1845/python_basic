b05b17707a5d8f967ccb2f53573704b6c4249789
hojin1845 <hojin1845@naver.com>
2022-09-04 19:24:58 +0900

    9.2.__init__

---
+++ 9.2.practice.py
@@ -0,0 +1,15 @@
+#9.2.__init__
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+#marine1 = Unit("마린", 40, 5)
+#marine2 = Unit("마린", 40, 5)
+#tank = Unit("탱크", 150, 35)
+
+
+marine3 = Unit("마린", 40)
