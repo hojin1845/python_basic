3109d6fda1bd48262fe20b4adaa065bcd6f06f8e
hojin1845 <hojin1845@naver.com>
2022-09-16 21:51:02 +0900

    10.2.에러발생시키기

--- 10.2.practice.py
+++ 10.2.practice.py
@@ -1,6 +1,10 @@
-#10.2.에러 발생시키기
+#10.2.에러발생시키기
 try:
-    print("한자리 수자 나누기 전용 계산기입니다.")
-    num1 = int(input("첫 번째 숫자를 입력하세요"))
-    num2 = int(input("두 번째 숫자를 입력하세요"))
-    if num1 >= 10 or num2 # 4:59 : 1
+    print("한 자리 숫자 나누기 전용 계산기 입니다.")
+    num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+    num2 = int(input("두 번째 숫자를 입력하세요 : "))
+    if num1 >= 10 or num2 >= 10:
+        raise ValueError
+    print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
+except ValueError
+    
