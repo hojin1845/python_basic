b688e89abdc056ef56092b234cbcb1242223b6c8
hojin1845 <hojin1845@naver.com>
2022-09-17 17:30:10 +0900

    10.2.에러발생시키기

--- 10.2.practice.py
+++ 10.2.practice.py
@@ -6,5 +6,6 @@ try:
     if num1 >= 10 or num2 >= 10:
         raise ValueError
     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
-except ValueError
+except ValueError:
+    print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
     
