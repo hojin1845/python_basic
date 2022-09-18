2da9fb93b1d8031ba6e44382310348c71fd0ffeb
hojin1845 <hojin1845@naver.com>
2022-09-18 10:30:25 +0900

    11.2.패키지

--- 11.2.practice.py
+++ 11.2.practice.py
@@ -1,6 +1,15 @@
 #11.2.패키지
-import travel.thiland
-trip_to = travel.thiland.ThilandPackage()
+#import travel.thiland
+#import travel.thiland.ThilandPackage
+#trip_to = travel.thiland.ThilandPackage()
+#trip_to.detail()
+
+#from travel.thiland import ThilandPackage
+#trip_to = ThilandPackage()
+#trip_to.detail()
+
+from travel import vietnam
+trip_to = vietnam.VietnamPackage()
 trip_to.detail()
 
 
