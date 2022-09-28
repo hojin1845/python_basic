21bc376be1ce57a295f4a39a79edd0c5833511bc
hojin1845 <hojin1845@naver.com>
2022-09-28 21:19:41 +0900

    10강 자료형 int,float

---
+++ 달려라파이썬.py/9-18.달려라파이썬10강.py
@@ -0,0 +1,34 @@
+#10강자료형 int,float
+#int : 정수
+#float : 실수
+# + - : 덧샘 뺄샘 : * ** : 곱샘 : / // % : 나눗샘
+#print('4 * 2 = ',4 * 2)
+#print('4 ** 3 =',4 ** 3)
+
+#print('50 / 4 = ', 50 / 4)
+#print('50 // 4 =', 50 // 4)
+#print('50 % 4 =',50 % 4)
+
+apple = 30
+orange = 10
+banana = 25
+student = 7
+
+#1번 과일 갯수 구하기
+fruit = apple + orange + banana
+print(fruit)
+
+# 2번 사과 - 오랜지 
+diff = apple - orange
+print(diff)
+
+#3번 
+# 1인당 바나나 갯수
+
+per = banana // student
+print(per)
+
+#나머지
+
+mod = banana % student
+print(mod)
