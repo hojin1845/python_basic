7bc5ab6514990ba226a3c9d9fc32456f08df6e2c
hojin1845 <hojin1845@naver.com>
2022-09-28 21:06:17 +0900

    입력과 출력

---
+++ 달려라파이썬.py/9-18.달려라파이썬9강.py
@@ -0,0 +1,19 @@
+#9강 입력과 출력
+#입력 --> 파이썬 --> 출력
+#input() : 입력함수 : name = input('너의 이름은 무엇이니?')
+#print() : 출력함수 : print('안녕', name)
+#name = input('너의 이름은 무엇이니?')
+#print('안녕',name)
+
+#number1 = input('첫번쨰 수:')
+#number2 = input('두번쨰 수:')
+#total = number1 + number2
+#print('number1, number2, total')
+
+# [연습문제]
+
+
+
+
+animal = input('너가 좋아하는 동물은 무엇이니?')
+print('내가 좋아하는 동물은',animal)
