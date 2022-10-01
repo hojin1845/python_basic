7538c8f8ac75fe84e2b2e293f4964360c8902232
hojin1845 <hojin1845@naver.com>
2022-10-01 10:17:26 +0900

    자료형 bool (1)

---
+++ 달려라파이썬.py/9-18.달려라파이썬13강.py
@@ -0,0 +1,34 @@
+#13강 자료형 bool (1)
+# 1 : True False : 참과 거짓을 나타내는 자료형
+#light_on = True
+#-2,-1,1,2,3... : True
+#0 : False
+#"라이언" : True
+#"" : False
+#[1,2,3,4,5] : True
+#[] : False
+#None : False
+
+#비교 연산자
+#>,<,>=,<=,==,!=
+#7 > 3 (True)
+#7 < 3 (False)  
+
+# a > b : a > b : a는b보다 크다
+# a < b : a < b : a는b보다 작다
+# a > b : a >= b : a는b보다 크거나같다
+# a < b : a <= b : a는b보다 작거나같다
+# a = b : a == b : a와b는같다
+# a =/ b : a !=b : a와b는같지않다
+
+#비교연산자
+#number = int(input("수를 입력하세요"))
+
+#print("10보다 큽니다",number > 10)
+#print("10보다 작습니다",number < 10)
+#print("10과 같습니다",number == 10)
+#print("10이 아닙니다",number != 10)
+
+#문자 비교하기
+name = input("이름을 입력하세요")
+print("이름은 라이온입니다", name == "라이온")
