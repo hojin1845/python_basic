d2d27510e52b3e9501482ab3c3ec1cc6d2a49e20
hojin1845 <hojin1845@naver.com>
2022-10-02 16:19:56 +0900

    if문

---
+++ 달려라파이썬.py/9-18.달려라파이썬22강.py
@@ -0,0 +1,11 @@
+#22강 if문 : 조건에 따라 선택적인 코드 실행
+#if number <= 10:   1 ~ 10 번인 경우
+# print("A팀")
+#elif number > 10 and  number <=20: 11번 ~ 20번인 경우
+#print("B팀")
+#else:                     나머지
+#print("C팀")
+
+
+
+
